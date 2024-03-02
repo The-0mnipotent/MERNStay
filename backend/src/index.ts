6d42cda7fe5d407909d696e4a320597269a73cf8
Arpit Singh <arpit84_soe@jnu.ac.in>
2024-03-02 19:33:16 +0530

    Add hotel Feature Added

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,4 +1,4 @@
-// import { v2 as cloudinary } from "cloudinary";
+import { v2 as cloudinary } from "cloudinary";
 import cookieParser from "cookie-parser";
 import cors from "cors";
 import "dotenv/config";
@@ -9,14 +9,14 @@ import path from "path";
 import authRoutes from "./routes/auth";
 // import hotelRoutes from "./routes/hotels";
 // import bookingRoutes from "./routes/my-bookings";
-// import myHotelRoutes from "./routes/my-hotels";
+import myHotelRoutes from "./routes/my-hotels";
 import userRoutes from "./routes/users";
 
-// cloudinary.config({
-//   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-//   api_key: process.env.CLOUDINARY_API_KEY,
-//   api_secret: process.env.CLOUDINARY_API_SECRET,
-// });
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
 
 const app = express();
 app.use(cookieParser());
@@ -33,12 +33,12 @@ app.use(express.static(path.join(__dirname, "../../frontend/dist")));
 
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
-// app.use("/api/my-hotels", myHotelRoutes);
+app.use("/api/my-hotels", myHotelRoutes);
 // app.use("/api/hotels", hotelRoutes);
 // app.use("/api/my-bookings", bookingRoutes);
 
 app.get("*", (req: Request, res: Response) => {
-  //   res.sendFile(path.join(__dirname, "../../frontend/dist/index.html"));
+  res.sendFile(path.join(__dirname, "../../frontend/dist/index.html"));
 });
 
 app.listen(7000, async () => {
